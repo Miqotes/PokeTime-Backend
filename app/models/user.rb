52c25288dc0eba0f908ee56124642a2c8074164a
Miqotes <ashleelennab@gmail.com>
2020-11-23 12:20:20 -0800

    building out more models and controllers

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
+
+    has_many :teams, through: :pokemon
+    has_many :favorites, through: :pokemon
 end
---
+++ db/migrate/20201123200819_create_teams.rb
@@ -0,0 +1,8 @@
+class CreateTeams < ActiveRecord::Migration[6.0]
+  def change
+    create_table :teams do |t|
+      t.references :user, null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end

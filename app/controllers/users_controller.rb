07824fcefe8fcc7e50412bf308f270bc6269d483
Miqotes <ashleelennab@gmail.com>
2020-11-22 22:01:49 -0800

    adding pokemon model/controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,9 +11,33 @@ class UsersController < ApplicationController
         end
     end
 
+    def index
+        users = User.all
+        render json: users, status: 200
+      end
+    
+      def update
+        @user.update(user_params)
+        render json: @user, status: 200
+      end
+    
+      def destroy
+        userId = @user.id
+        @user.destroy
+        render json: {message:"User account deleted", userId:userId}
+      end
+    
+      def show
+        render json: @user, status: 200
+      end
+
     private
 
     def user_params
         params.permit(:username, :password)
     end
+
+    def set_user
+        @user = User.find(params[:id])
+      end
 end

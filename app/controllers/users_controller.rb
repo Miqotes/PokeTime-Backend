01f6cc0872d7d188fac20edb6a2848ada9d60bfc
Miqotes <ashleelennab@gmail.com>
2020-11-22 17:46:45 -0800

    Finished auth/application controllers for login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,19 @@
 class UsersController < ApplicationController
+
+    def Create
+        user = User.create(user_params)
+        if user.valid?
+            payload = { user_id: user.id }
+            token = encode_token(payload)
+            render json: { user: user, jwt: token }
+        else
+            render json: { errors: user.errors.full_messages }, status: :not_acceptable
+        end
+    end
+
+    private
+
+    def user_params
+        params.permit(:username, :password)
+    end
 end
